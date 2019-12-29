126ef0b8ab12941db0b6db0ee401337aa05d8eaf
superbear <superbear@DN030.local>
2019-12-29 11:20:56 +0800

    add install docker

---
+++ install_docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# install dependencies
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+# config repository
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+# install 
+yum install docker-ce
+
+# start and enable
+systemctl start docker \
+  && systemctl enable docker
+
+# config image repository
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+systemctl restart docker
