4d360c7690db7047d351e3611eb2bac747df328f
superbear <superbear@DN030.local>
2019-12-29 17:14:12 +0800

    add privilege

--- install/docker/jenkins.sh
+++ install/docker/jenkins.sh
@@ -5,10 +5,12 @@ set -e
 JENKINS_VERSION=2.204
 VOLUME_DIRECTORY=/mnt/dockerVolumn/jenkins
 
-# install
+# modifuy volumn permissions
+chown -R 1000:1000 ${VOLUME_DIRECTORY}
 
+# install
 docker run --name jenkins \
 -p 8080:8080 -p 50000:50000 \
 -v ${VOLUME_DIRECTORY}:/var/jenkins_home \
-jenkins/jenkins:${JENKINS_VERSION}
+-d jenkins/jenkins:${JENKINS_VERSION}
 
